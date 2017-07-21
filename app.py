fe1d2213ae4133be438e6639d4e129d67a9558b2
Tobias P <Tobias@virtual-pahl.de>
2017-07-21 11:16:24 +0200

    runnable

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-// ./app.py
+# ./app.py
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-	return 'Yo, it's working!'
+	return 'Yo, its working!'
 if __name__ == "__main__":
-	app.run()
+	from os import environ
+	app.run(debug=True, host='0.0.0.0', port=int(environ.get("PORT", 5000)))
+	
